56ba761208f80fa753d5fa3c8a21704a669628d1
James McCann <jamesredux@gmail.com>
2017-03-18 15:24:05 +0700

    saturday commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,85 @@
 $(document).ready(function()  {
+	drawGrid(32);
 
 
-var numSquare = 16;	
-function addDiv()  {
-	for (i = 1; i <= numSquare * numSquare; i++)  {
-		$(".wrapper").append($("<div></div>").addClass('square'));
-	}
-	
-var totalSquare = numSquare * numSquare;
-var squareSize = 600 / numSquare;
-$(".square").css({'height': squareSize, "width": squareSize});
-}
-addDiv();
-
-$(".square").mouseenter(function ()  {
-	$(this).addClass('mouse');
-})
-//This works but don'd want to use it yet
-//.mouseleave(function() {
-//	$(this).removeClass('mouse');
-//})
-function newGrid() {
+function drawGrid(numSquare)  {
 	$(".square").remove();
-	var numSquare = prompt("How many square do you want", "16")
-	function changeDiv()  {
+	var squareSize = 600 / numSquare;
+
 	for (i = 1; i <= numSquare * numSquare; i++)  {
 		$(".wrapper").append($("<div></div>").addClass('square'));
-	}
+
+	}	
+	$(".square").css({'height': squareSize, "width": squareSize});
 	
-var totalSquare = numSquare * numSquare;
-var squareSize = 600 / numSquare;
-$(".square").css({'height': squareSize, "width": squareSize});
-}
-changeDiv();
+
+	$(".square").mouseenter(function ()  {
+	$(this).css({"background-color": "#5e8be5"});
+	});
 
 }
 
-$('#Bone').on('click', function() {
-	newGrid();
-});
-//add check for correct number
-//add var to change grid size
+function newGrid() {
+	var requestNumber = prompt("How many squares do you want? Choose from 1-64", "32")
+	var numSquare = requestNumber;
+	if (isNaN(numSquare))  {
+		alert("Choose a real number please")
+		newGrid();
+	}
+
+	else if (numSquare <1 || numSquare > 64)  {
+		alert("I SAID CHOOSE A SIZE BETWEEN 1-64")
+		newGrid();
+	}	
+	else {
+	drawGrid(numSquare);
+}
+
+}
+	
+$('#Bone').on('click', function(){
+		newGrid();
+
+}); 
+
+//Random Color.
+
+$('#Btwo').on('click', function(){
+		
+		newGrid();
+		$(".square").mouseenter(function ()  {
+	$(this).css({"background-color": randomColor()});
+	});
 
 });
+function randomColor() {
+	var digits = ['0','1','2','3','4','5','6','7','8',
+	'9','A', 'B', 'C', 'D', 'E', 'F'],
+	 ranColor = '#', i;
+	for (i=0; i < 6; i++)  {
+		ranColor = ranColor += digits[Math.floor(Math.random() * 16)];
+	}
+	return ranColor;
+}
+//end of randomColor function.
+
+//picture
+
+$('#Bthree').on('click', function(){
+	 $('.wrapper').addClass("jazz");
+	 drawGrid(64);
+	 $(".square").mouseenter(function ()  {
+	$(this).css({"background-color": 'transparent'});
+	});
+
+	});
+
+
+//end of piicture
+
+//gradient to do
+
+//lastbrackets below!!
+});
+
+
